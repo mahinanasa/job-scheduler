57f6fac2b8890f45f38c30277fda636b0a936dbd
mahinanas <mahinanas@gmail.com>
2025-03-17 11:15:01 +0400

    added trusted host

--- job_scheduler/settings.py
+++ job_scheduler/settings.py
@@ -97,6 +97,8 @@ REST_FRAMEWORK = {
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
+    "https://job-scheduler-smm7.onrender.com",
+    "https://job-scheduler-smm7.onrender.com",
 ]
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
